6a47e42bb24d4ef6e459c77b7f3bc35573283ece
Vinicius Mendite <vinicius.miff@gmail.com>
2020-04-21 14:12:27 -0300

    Configuração do sequelize pronta

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: '47289',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
