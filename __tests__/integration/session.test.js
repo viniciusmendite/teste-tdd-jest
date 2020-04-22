b263b74e4365cc0db4dd4ab39688f1d02fb8239c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-04-21 21:29:45 -0300

    Comparando hash e fazendo teste unitário

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -16,9 +16,35 @@ describe('Authentication', () => {
       password: '1234456',
     });
     const response = await request(app).post('/sessions').send({
-      name: user.email,
-      password: '123456',
+      email: user.email,
+      password: '1234456',
     });
     expect(response.status).toBe(200);
+  });
+
+  it('should not authenticate with invalid credentials', async () => {
+    const user = await User.create({
+      name: 'Vinícius Mendite',
+      email: 'vinicius.47289@outlook.com',
+      password: '1234456',
+    });
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '18934',
+    });
+    expect(response.status).toBe(401);
+  });
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await User.create({
+      name: 'Vinícius Mendite',
+      email: 'vinicius.47289@outlook.com',
+      password: '123456',
+    });
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '123456',
+    });
+    expect(response.body).toHaveProperty('token');
   });
 });
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,19 @@
+const { User } = require('../models');
+
 module.exports = {
   async store(req, res) {
-    return res.status(200).send();
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ message: 'Incorrect password' });
+    }
+
+    return res.json({ user });
   },
 };
